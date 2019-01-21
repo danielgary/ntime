3390bc312028d611f473293217da9a9f4f98dd2c
Daniel Gary <daniel@gatewayapps.com>
2019-01-21 15:21:27 -0600

    added -s and -u args

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# ntime
+# ntimer
+
+Time the execution of a command
+
+```
+Usage: ntimer -s <command to time>
+
+Options:
+  --help                              Show help                        [boolean]
+  --version                           Show version number              [boolean]
+  -s, --Suppress output from command                  [boolean] [default: false]
+  -u, --Units of time
+      [choices: "milliseconds", "seconds", "minutes", "hours", "days"] [default:
+                                                                      "seconds"]
+```
+
+
+Example
+```
+> ntimer -s -u milliseconds curl www.google.com
+curl www.google.com  completed in 186 milliseconds
+
+> ntimer -s -u seconds curl www.github.com
+curl www.github.com  completed in 0.251 seconds
+
+```
+
--- src/ntimer.ts
+++ src/ntimer.ts
@@ -2,17 +2,35 @@
 import childProcess from 'child_process'
 import moment from 'moment'
 import chalk from 'chalk'
+import yargs from 'yargs'
 
-const ntimeArgs = process.argv
-ntimeArgs.splice(0, 2)
+let ntimerArgs = yargs
+  .usage(`Usage: ntimer -s <command to time>`)
+  .alias('s', 'Suppress output from command')
+  .alias('u', 'Units of time')
 
-const command = ntimeArgs.join(' ')
+  .version()
+  .boolean('s')
+  .option('s')
+  .choices('u', ['milliseconds', 'seconds', 'minutes', 'hours', 'days'])
+  .default('u', 'seconds')
+  .default('s', false).argv
+
+const command = ntimerArgs._.join(' ')
+
+if (!command) {
+  process.stdout.write('You must provide a command to execute\n')
+  process.exit(1)
+}
 
 const startTime = moment()
 
+const stdio = ntimerArgs.s ? [undefined, undefined, undefined] : 'inherit'
+const silent = ntimerArgs.s
+
 //process.stdout.write(`Process started at ${currentTime.format}`)
-childProcess.execSync(command, { stdio: 'inherit' })
+childProcess.execSync(command, { stdio })
 const finishTime = new Date()
 const m_finishTime = moment(finishTime)
 //process.stdout.write(`Process completed at ${finishTime.toLocaleTimeString()}`)
-process.stdout.write(`${chalk.bgGreen(chalk.black(' ' + command + ' '))} completed in ${m_finishTime.diff(startTime, 'seconds', true)} seconds\n`)
+process.stdout.write(`${chalk.bgGreen(chalk.black(' ' + command + ' '))} completed in ${m_finishTime.diff(startTime, ntimerArgs.u, true)} ${ntimerArgs.u}\n`)
