43dfae83b36300ee5c2237cd18adf4e640648e6f
Daniel Gary <daniel@gatewayapps.com>
2019-01-21 13:45:46 -0600

    renamed ntime to ntimer

---
+++ dist/ntimer.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const child_process_1 = __importDefault(require("child_process"));
+const moment_1 = __importDefault(require("moment"));
+const chalk_1 = __importDefault(require("chalk"));
+const ntimeArgs = process.argv;
+ntimeArgs.splice(0, 2);
+const command = ntimeArgs.join(' ');
+const startTime = moment_1.default();
+//process.stdout.write(`Process started at ${currentTime.format}`)
+child_process_1.default.execSync(command, { stdio: 'inherit' });
+const finishTime = new Date();
+const m_finishTime = moment_1.default(finishTime);
+//process.stdout.write(`Process completed at ${finishTime.toLocaleTimeString()}`)
+process.stdout.write(`${chalk_1.default.bgGreen(chalk_1.default.black(' ' + command + ' '))} completed in ${m_finishTime.diff(startTime, 'seconds', true)} seconds\n`);
+//# sourceMappingURL=ntimer.js.map
